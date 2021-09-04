7868aa9e30205ea9220ee2e0790fb933c7b7fd44
Sayeh Hiba <sayeh_hiba@yahoo.com>
2021-09-04 15:03:26 +0100

    Sound-In-Windows-Forms-app

---
+++ alarm/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace alarm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::alarm.Properties.Resources.m;
+            this.pictureBox1.Location = new System.Drawing.Point(353, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(374, 406);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mistral", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(113, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 48);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "température";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(138, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 52);
+            this.label2.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(26, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(759, 452);
+            this.panel1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(79, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 61);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Démarrer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 503);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ alarm/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Collections;
+using System.Media;
+namespace alarm
+{
+    public partial class Form1 : Form
+    {
+      machine m = new machine();
+      List<String> c = new List<String>();
+      
+        public Form1()
+        {
+            InitializeComponent();
+            m.EtatAlarm += sonner;
+            remplir();
+        }
+
+        public void remplir()
+        {
+            c.Add("80");
+            c.Add("90");
+            c.Add("100");
+            c.Add("120");
+            c.Add("95");
+            c.Add("100");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            for (int i = 0; i < 6; i++)
+            {
+
+                label2.Text = c[i];
+               
+                m.Temperature = Convert.ToInt32(c[i]); 
+                
+                
+                Thread.Sleep(2000);
+                
+                
+            }
+        }
+        
+       public class TempsArgs : EventArgs
+        {
+            int tps;
+
+            public int Tps
+            {
+                get { return tps; }
+                set { tps = value; }
+            }
+            public TempsArgs(int t)
+            {
+                tps = t;
+            }
+        }
+        public class machine
+        {
+            public event EventHandler<TempsArgs> EtatAlarm;
+            private int _temperature;
+
+            public int Temperature
+            {
+                get { return _temperature; }
+                set
+                {
+                    
+                    _temperature = value;
+
+                    if (EtatAlarm != null)
+                        EtatAlarm(this, new TempsArgs(value));
+                }
+            }
+
+        }
+
+
+       
+            public void sonner(object sender, TempsArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer("a.wav");
+                if (e.Tps >= 100)
+                {
+                   
+                    sp.Play();
+                    panel1.BackgroundImage = Image.FromFile("2.jpg");
+
+                    MessageBox.Show("temperature depasse 100");
+                }
+                else
+                {
+                    sp.Stop();
+                    panel1.BackgroundImage=Image.FromFile("1.jpg");
+                    MessageBox.Show("temperature normal");
+                }
+            }
+
+
+          
+        
+      
+    }
+}
